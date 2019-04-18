cb308a9287fcf19baebd43c01d503907a6ba88b6
Nicko229 <you@example.com>
2019-04-18 12:21:14 +0200

    axios working for generic picture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      picture: ''
+    }
+  }
+
+
+  componentDidMount = () => {
+    axios.get('https://dog.ceo/api/breed/Affenpinscher/images/random')
+      .then(res => {
+        console.log(res);
+        this.setState({ picture: res.data });
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <img src={this.state.picture.message} alt="dog picture" />
       </div>
     );
   }
