c443d080e82c8cfbd4bb6a64942f6bd413196e71
Nicko229 <you@example.com>
2019-04-20 18:08:51 +0200

    media query added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Banner from './banner';
 import placeholder from './placeholder.jpg';
 import './App.css';
 
-
 class App extends Component {
   constructor(props) {
     super(props)
@@ -18,14 +17,12 @@ class App extends Component {
     this.setState({ breed: e.target.value })
   }
 
-
   handleSubmit = (e) => {
     e.preventDefault()
     axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images/random`)
       .then(res => {
         console.log(res);
         this.setState({ picture: res.data });
-        console.log("picture: ", this.state.picture)
       })
       .catch(error => {
         console.log(error)
@@ -36,22 +33,18 @@ class App extends Component {
     return (
       <div className="main-div">
         <Banner />
-
-
         <form onSubmit={this.handleSubmit} className="dog-form">
           <div className="search-div">
             <h3>Type in your favourite dog breed to see pictures of your future pets</h3>
-            <input type="text" onChange={this.handleChange} />
+            <input className="text-input" type="text" onChange={this.handleChange} />
             <br />
-            <input type="submit" value="Submit" />
+            <input className="submit-input" type="submit" value="Submit" />
           </div>
           <div className="image-div">
-            {console.log(this.state.picture)}
             <img className="dog-image" src={
               this.state.picture === '' ? placeholder : this.state.picture.message
             } />
           </div>
-
         </form>
       </div>
     );
